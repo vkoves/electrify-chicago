573b8d31508c86068cb863745873187726367819
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-06-08 18:37:53 -0500

    made data hold averages for all categories, not jsut the last

--- src/data/scripts/add-context-by-property-type.py
+++ src/data/scripts/add-context-by-property-type.py
@@ -15,17 +15,24 @@ building_cols_to_rank = [
     'SiteEUI',
 ]
 
+#raw building data
 building_data = pd.read_csv(path_to_buildings_csv)
+
+#sorted data based on each property type: the order is alphabetical
 sorted_by_property_type = building_data.groupby("PrimaryPropertyType")
 
 
 def calculateBuildingStatistics():
     building_staistics_by_property_type = {}
-    detail_cols_to_keep = ['count', 'mean', 'min', 'max', '25%', '50%', '75%']
     all_property_types = pd.read_json("./dist/property-types.json")
 
+    #looping through both all the property types and all teh columns we want to get data on
     for i, property in enumerate(all_property_types["propertyTypes"]):
+
+        cur_property_type_stats = {}
         for col in building_cols_to_rank:
+
+            #finding the mean, count, min, max, and quartiles of each category for each building type
             cur_mean = round(sorted_by_property_type[col].mean()[i].item(), 3)
             cur_count = round(
                 sorted_by_property_type[col].count()[i].item(), 3)
@@ -39,9 +46,11 @@ def calculateBuildingStatistics():
                 sorted_by_property_type[col].quantile(q=0.5)[i].item(), 3)
             cur_third_quartile = round(
                 sorted_by_property_type[col].quantile(q=0.75)[i].item(), 3)
-
-        building_staistics_by_property_type[property] = {"mean": cur_mean, "count": cur_count, "min": cur_min, "max": cur_max,
+            
+            cur_property_type_stats[col] = {"mean": cur_mean, "count": cur_count, "min": cur_min, "max": cur_max,
                                                          "twentyFifthPercentile": cur_first_quartile, "median": cur_median, "seventyFifthPercentile": cur_third_quartile}
+
+        building_staistics_by_property_type[property] = cur_property_type_stats
 
     with open("./dist/building-statistics-by-property-type.json", "w") as property_stats_file:
         json.dump(building_staistics_by_property_type, property_stats_file)
