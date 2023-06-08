8d6de16a1967414d9fdaa65ff373f91445a156d2
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-06-08 18:26:37 -0500

    created the data statistics for each property type

--- src/data/scripts/add-context-by-property-type.py
+++ src/data/scripts/add-context-by-property-type.py
@@ -18,15 +18,37 @@ building_cols_to_rank = [
 building_data = pd.read_csv(path_to_buildings_csv)
 sorted_by_property_type = building_data.groupby("PrimaryPropertyType")
 
+
 def calculateBuildingStatistics():
     building_staistics_by_property_type = {}
-    sorted_by_property_type = building_data.groupby("PrimaryPropertyType")
+    detail_cols_to_keep = ['count', 'mean', 'min', 'max', '25%', '50%', '75%']
+    all_property_types = pd.read_json("./dist/property-types.json")
 
-    for col in building_cols_to_rank:
-        print(col, sorted_by_property_type[col].mean())
+    for i, property in enumerate(all_property_types["propertyTypes"]):
+        for col in building_cols_to_rank:
+            cur_mean = round(sorted_by_property_type[col].mean()[i].item(), 3)
+            cur_count = round(
+                sorted_by_property_type[col].count()[i].item(), 3)
+
+            cur_min = round(sorted_by_property_type[col].min()[i].item(), 3)
+            cur_max = round(sorted_by_property_type[col].max()[i].item(), 3)
+
+            cur_first_quartile = round(
+                sorted_by_property_type[col].quantile(q=0.25)[i].item(), 3)
+            cur_median = round(
+                sorted_by_property_type[col].quantile(q=0.5)[i].item(), 3)
+            cur_third_quartile = round(
+                sorted_by_property_type[col].quantile(q=0.75)[i].item(), 3)
+
+        building_staistics_by_property_type[property] = {"mean": cur_mean, "count": cur_count, "min": cur_min, "max": cur_max,
+                                                         "twentyFifthPercentile": cur_first_quartile, "median": cur_median, "seventyFifthPercentile": cur_third_quartile}
+
+    with open("./dist/building-statistics-by-property-type.json", "w") as property_stats_file:
+        json.dump(building_staistics_by_property_type, property_stats_file)
 
 
 def rankByType():
+    calculateBuildingStatistics()
     building_data = pd.read_csv(path_to_buildings_csv)
 
     for col in building_cols_to_rank:
@@ -38,5 +60,4 @@ def rankByType():
 
 
 if __name__ == "__main__":
-    # rankByType()
-    calculateBuildingStatistics()
+    rankByType()
