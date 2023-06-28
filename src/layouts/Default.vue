9856e0bc12e508ef5fd8364c01a3d93a38542ae6
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2023-06-27 19:49:17 -0500

    Add Google Analytics Script

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -31,6 +31,19 @@ export default class Default extends Vue {
 
 <template>
   <div>
+    <!-- Google tag (gtag.js) -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-D4F03H5C02"
+    />
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-D4F03H5C02');
+    </script>
+
     <div class="layout">
       <!-- TODO: Split to a component-->
       <header class="header">
