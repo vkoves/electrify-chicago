e35eb1d006de1e2ea4a7d404455e3103489648e5
alexkcode <alexiskwan.uiuc@gmail.com>
2023-12-12 20:27:27 -0600

    script skeleton

---
+++ src/data/scripts/clean_geo_data.py
@@ -0,0 +1,13 @@
+import geopandas as gpd
+
+data_directory = './source/'
+building_emissions_file = 'ChicagoEnergyBenchmarking.csv'
+data_out_file = building_emissions_file
+
+ward_col = ['Wards']
+
+def map_address_to_ward():
+    pass
+
+if __name__ == "__main__":
+    pass
