3911ecc7a804aad733eead109badaa16960724ea
SuragNuthulapaty <suragn30@gmail.com>
2024-01-10 00:54:59 -0600

    added city of chicago buildings, found by matching addresses

--- src/constants/buildings-custom-info.constant.vue
+++ src/constants/buildings-custom-info.constant.vue
@@ -1171,6 +1171,30 @@ export const BuildingsCustomInfo: { [buildingSlug: string]: IBuildingCustomInfo 
    * City of Chicago
    * No helpful resource, but Googling each of these will pull up a Northwestern page
    */
+  // Chicago Indoor Sports
+  120015: { owner: BuildingOwners.cps.key },
+  // Damen Marketplace
+  254152: { owner: BuildingOwners.cps.key },
+  // City Hall
+  250115: { owner: BuildingOwners.cps.key },
+  // Harold Washington Library
+  157964: { owner: BuildingOwners.cps.key },
+  // Police Training Academy
+  250068: { owner: BuildingOwners.cps.key },
+  // Martin Luther King Community Center
+  250058: { owner: BuildingOwners.cps.key },
+  // Sulzer Library
+  250065: { owner: BuildingOwners.cps.key },
+  // Lake Village East Apartments
+  252070: { owner: BuildingOwners.cps.key },
+  // Museum of Science and Industry
+  102460: { owner: BuildingOwners.cps.key },
+  // Englewood Health Center
+  250057: { owner: BuildingOwners.cps.key },
+  // Gallery 37
+  250063: { owner: BuildingOwners.cps.key },
+  // CDOT Central Office
+  250066: { owner: BuildingOwners.cps.key },
 };
 
 export function getBuildingCustomInfo(building: IBuilding): IBuildingCustomInfo | null {
