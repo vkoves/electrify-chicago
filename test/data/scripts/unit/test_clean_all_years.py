a5d94b435b33028cc62a9969063e836d0306908c
alexkcode <alexiskwan.uiuc@gmail.com>
2024-01-16 21:45:43 -0600

    working on fixtures

--- test/data/scripts/unit/test_clean_all_years.py
+++ test/data/scripts/unit/test_clean_all_years.py
@@ -5,19 +5,19 @@ from src.data.scripts import clean_and_pare_down_data_all_years as clean
 
 src_dir = "..../src/data/source"
 test_src_dir = "..../test/data/source"
+file_to_copy = "ChicagoEnergyBenchmarking.csv"
+
+@pytest.fixture
+def get_dest_file():
+    os.path.join(test_src_dir, file_to_copy)
 
 @pytest.fixture
 def copy_file():
-    file_to_copy = "ChicagoEnergyBenchmarking.csv"
     src_file_path = os.path.join(src_dir, file_to_copy)
     dest_file_path = os.path.join(test_src_dir, file_to_copy)
     if not os.path.exists(src_file_path):
         shutil.copy(src_file_path, dest_file_path)
     return file_to_copy
-
-@pytest.fixture
-def get_header():
-    pass
 
 @pytest.fixture
 def building_data(copy_file):
@@ -31,4 +31,5 @@ def building_data(copy_file):
 def test_is_not_empty(test_input):
     assert len(test_input) > 0
 
-
+def test_main():
+    pass
