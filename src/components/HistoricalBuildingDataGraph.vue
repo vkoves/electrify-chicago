9a378c319c8f1203fe7d6e28912dedf4a880746f
alexkcode <alexiskwan.uiuc@gmail.com>
2024-06-11 21:13:27 -0500

    skeleton for d3 graphs

---
+++ src/components/HistoricalBuildingDataGraph.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="historical-graph-cont">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import {IHistoricData} from '../common-functions.vue';
+
+@Component({
+    
+})
+</script>
