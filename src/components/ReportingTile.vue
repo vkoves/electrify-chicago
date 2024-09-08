712c17f8425df51c0ddd1633e21b71e073c56c25
noceo <paulschade20@gmail.com>
2024-09-08 17:15:17 -0500

    Add score and grade

--- src/components/ReportingTile.vue
+++ src/components/ReportingTile.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="reporting-tile">
+    <p>
+      Years Reported
+      <span class="score"
+        >{{ reportedYears }}/{{ reportingHistory.length }}</span
+      >
+      <span>{{ grade }}</span>
+    </p>
     <ul>
       <li v-for="item in reportingHistory" :key="item.year">
         {{ item.year }} - {{ item.isReported }}
@@ -19,6 +26,23 @@ export default class ReportingTile extends Vue {
   @Prop() historicData?: Array<IHistoricData>;
 
   readonly LatestDataYear: number = LatestDataYear;
+
+  get grade(): string {
+    const gradeRanges = [
+      { min: 0.9, grade: "A" },
+      { min: 0.8, grade: "B" },
+      { min: 0.7, grade: "C" },
+      { min: 0.6, grade: "D" },
+      { min: 0, grade: "F" },
+    ];
+
+    const score = this.reportedYears / this.reportingHistory.length;
+    return gradeRanges.find((range) => score >= range.min)!.grade;
+  }
+
+  get reportedYears(): number {
+    return this.reportingHistory.filter((entry) => entry.isReported).length;
+  }
 
   get reportingHistory(): { year: number; isReported: boolean }[] {
     if (!this.historicData || this.historicData.length === 0) return [];
