24e03dca0654bf431e77d3ae3668d6c7aa4672d6
noceo <paulschade20@gmail.com>
2024-09-08 21:25:29 -0500

    Add markers that indicate the reporting status for every year until the latest year of data + styling

--- src/components/ReportingTile.vue
+++ src/components/ReportingTile.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="reporting-tile">
-    <p>
+    <p class="headline">
       Years Reported
       <span class="score"
         >{{ reportedYears }}/{{ reportingHistory.length }}</span
       >
-      <span>{{ grade }}</span>
+      <span class="grade">{{ grade }}</span>
     </p>
     <ul>
-      <li v-for="item in reportingHistory" :key="item.year">
-        {{ item.year }} - {{ item.isReported }}
+      <li
+        class="reporting-tile-item"
+        v-for="item in reportingHistory"
+        :key="item.year"
+      >
+        <div class="marker" :class="{ '-reported': item.isReported }">
+          <img
+            v-if="item.isReported"
+            src="/checkmark.svg"
+            :alt="`${item.year} data reported`"
+            class="reported"
+          />
+          <img
+            v-else
+            src="/cross.svg"
+            :alt="`${item.year} data not reported`"
+          />
+        </div>
+        <p>{{ item.year }}</p>
       </li>
     </ul>
   </div>
@@ -75,4 +92,74 @@ export default class ReportingTile extends Vue {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.reporting-tile {
+  .headline {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+
+    .score {
+      font-size: initial;
+      font-weight: bold;
+      margin-right: 1rem;
+    }
+
+    .grade {
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem 2rem;
+    list-style: none;
+    padding: 0;
+
+    .reporting-tile-item {
+      margin-top: 0;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .marker {
+    position: relative;
+    width: 40px;
+    height: 40px;
+    margin-bottom: 0.25rem;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 85%;
+      height: 85%;
+      border: 0.225rem solid #767676;
+      border-radius: 50%;
+      z-index: -1;
+    }
+
+    img {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      height: 100%;
+    }
+
+    &.-reported {
+      img {
+        top: 40%;
+        left: 62%;
+      }
+    }
+  }
+}
+</style>
