0e13f596a6048f2a119ff8e30b8cc8f44713fd28
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2025-04-22 21:36:29 -0500

    Fix GHG intensity not calculating with historic data

--- src/data/scripts/grade_buildings.py
+++ src/data/scripts/grade_buildings.py
@@ -57,6 +57,9 @@ def generate_percentile_grade(
     grade of 56.37 means this building is better than 56.37% of records in
     `vals`.
 
+    NOTE: Handles NaN values by omitting them, so it doesn't error due to buildings missing
+    some data.
+
     Parameters
     ----------
     vals : pd.Series
@@ -78,13 +81,14 @@ def generate_percentile_grade(
     """
     grades = pd.DataFrame(index=vals.index)
 
-    # Calculate percentile-based score out 100:
+    # Calculate percentile-based score out 100, ignoring NaN values
     if reverse:
         def calc_func(x):
-            return 100 - percentileofscore(vals, x, kind="weak")
+            return 100 - percentileofscore(vals, x, kind="weak", nan_policy="omit")
     else:
         def calc_func(x):
-            return percentileofscore(vals, x, kind="weak")
+            return percentileofscore(vals, x, kind="weak", nan_policy="omit")
+
     percent_scores: pd.Series = vals.apply(calc_func)
     grades[f"{col_base_name}PercentileGrade"] = percent_scores
 
@@ -176,15 +180,18 @@ def apply_grade_func_all_years(df, func):
     """
     all_years = df["DataYear"].unique()
     grades_all_years = []
+
     for year in all_years:
         grades_df = func(
             df=df, year=year
         )
+
         grades_all_years.append(grades_df)
 
     grades_all_years_df = pd.concat(
         grades_all_years
     )
+
     return grades_all_years_df
 
 
@@ -409,12 +416,16 @@ def grade_ghg_intensity_energy_mix_all_years(building_data: pd.DataFrame):
         on=["ID", "DataYear"],
     )
 
+    print('grades_all_yeras', grades_all_years_df[['GHGIntensityPercentileGrade', 'GHGIntensityLetterGrade']].head())
+
     # Add to the original dataset:
     df = pd.merge(
         building_data,
         grades_all_years_df,
         on=["ID", "DataYear"],
     )
+
+    print(building_data.head())
 
     return df
 
@@ -441,7 +452,7 @@ def calculate_weighted_average(graded_df: pd.DataFrame) -> pd.Series:
 
 def grade_buildings():
     df_historical = pd.read_csv(data_in_file_historical_path)
-   
+
     # Generate grades for all years for GHG Intensity and Energy Mix:
     graded_df = grade_ghg_intensity_energy_mix_all_years(
         building_data=df_historical,
