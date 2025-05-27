960a2066594b11112df76b4ac1a803e1d3d7627e
wangbertha <wangbertha@gmail.com>
2025-05-27 17:17:44 -0500

    Add tests for WardsByCityGeocoder building attr.

---
+++ tests/data/scripts/unit/test_find_ward.py
@@ -0,0 +1,25 @@
+import pytest
+import pandas as pd
+from src.data.scripts.utils import get_data_file_path
+from src.data.scripts.add_ward_numbers import compare_ward_methods
+
+building_path = get_data_file_path("dist", "building-benchmarks.csv")
+building_benchmarks = pd.read_csv(building_path)
+
+property_ids_to_include = [
+    100856,   # United Center
+    256419,   # Crown Hall
+    160196,   # The Art Institute of Chicago
+    138730,   # random property
+    251245,   # random property
+    109613,   # random property
+]
+
+def test_sample_buildings_with_wards():
+    # Test WardByCityGeocoder building benchmark attribute against the City of Chicago's
+    # 'Find My Ward and Alderman page':
+    # https://www.chicago.gov/city/en/depts/mayor/iframe/lookup_ward_and_alderman.html
+    building_benchmarks_ordered = building_benchmarks.set_index('ID')
+    test_data = building_benchmarks_ordered.loc[property_ids_to_include]
+    expected_wards = [27, 3, 42, 20, -1, 24]
+    assert test_data['WardByCityGeocoder'].to_list() == expected_wards
