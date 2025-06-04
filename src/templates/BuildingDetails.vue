e3e115e35d6d190daaa1a18c5ef5506956f7a60e
wangbertha <wangbertha@gmail.com>
2025-06-04 00:12:02 -0500

    Fix display when building does not have Ward

--- src/templates/BuildingDetails.vue
+++ src/templates/BuildingDetails.vue
@@ -229,12 +229,12 @@ query ($id: ID!, $ID: String) {
 
                 <div>
                   <dt>Ward</dt>
-                  <dd v-if="building.Ward !== -1">
+                  <dd v-if="parseInt(building.Ward) !== -1">
                     <g-link class="nav-link" :to="`/ward/${building.Ward}`">
                       {{ building.Ward }}
                     </g-link>
                   </dd>
-                  <dd v-if="building.Ward === -1">Not found</dd>
+                  <dd v-else>Not found</dd>
                 </div>
 
                 <!-- Show energy rating if it's a float value (not blank or NaN) -->
