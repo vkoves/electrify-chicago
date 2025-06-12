0740ff4434e8c99f70e4d02ca20c2b98128d0129
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2025-06-12 13:43:33 -0500

    Fix long line

--- src/templates/BuildingDetails.vue
+++ src/templates/BuildingDetails.vue
@@ -625,7 +625,8 @@ Vue.use(vToolTip);
     const emissions = Math.round(
       this.$page.building.TotalGHGEmissions || 0,
     ).toLocaleString();
-    const description = `${propertyName} in Chicago - Grade ${grade} building with ${emissions} tons CO₂e emissions.`;
+    const description = `${propertyName} in Chicago - `
+      + `Grade ${grade} building with ${emissions} tons CO₂e emissions.`;
     const socialImageUrl = `/social-images/building-${this.$page.building.ID}.png`;
 
     return {
