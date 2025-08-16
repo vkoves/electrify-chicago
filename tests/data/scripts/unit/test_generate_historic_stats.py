72e6088ab6bb279bbb70febacc44ecd630036c89
hunter <hunter@hunters-MacBook-Pro.local>
2025-08-16 05:42:59 -0500

    setting up testing

---
+++ tests/data/scripts/unit/test_generate_historic_stats.py
@@ -0,0 +1,9 @@
+# import pytest
+# import pandas as pd
+# from pandas.testing import assert_series_equal
+# from unittest.mock import patch
+# from src.data.scripts.generate_historic_stats import (
+#     calculateBuildingStatsByYear
+# )
+
+# def test_
