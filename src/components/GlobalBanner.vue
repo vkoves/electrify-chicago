ccb4c69c8e272d818002128c554e109534cdc689
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2025-12-29 17:41:12 -0600

    Migrate global banner SCSS

--- src/components/GlobalBanner.vue
+++ src/components/GlobalBanner.vue
@@ -29,3 +29,92 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({})
 export default class GlobalBanner extends Vue {}
 </script>
+
+<style lang="scss">
+  /**
+ * Global banner - notice panel for important announcements and calls to action
+ */
+.global-banner {
+  position: relative;
+  background: $blue-very-dark;
+  padding: 1rem 0;
+  box-shadow: 0 0.3125rem 0.25rem $box-shadow-main;
+  width: 100%;
+  z-index: 1;
+
+  // Add margin and border-radius for non-full-width layouts
+  .layout:not(.-full-width) & {
+    margin: 0 1rem 2rem;
+    border-radius: $brd-rad-medium;
+    width: calc(100% - 2rem);
+  }
+
+  .page-constrained {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  .content-left {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    flex-grow: 1;
+  }
+
+  h2 {
+    color: $white;
+    font-size: 1.25rem;
+    margin: 0;
+  }
+
+  p {
+    color: $white;
+    font-size: 0.875rem;
+  }
+
+  a {
+    background-color: $white;
+    padding: 0.5rem 1.25rem;
+    border-radius: $brd-rad-small;
+    text-align: center;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 0.9rem;
+
+    &:focus {
+      outline-color: $white;
+    }
+    &:hover,
+    &:focus {
+      background-color: $off-white;
+    }
+  }
+
+  /** Mobile styling */
+  @media (max-width: $mobile-max-width) {
+    padding: 0.75rem 0;
+    text-align: center;
+
+    .page-constrained {
+      gap: 0.5rem;
+      flex-direction: column;
+    }
+
+    h2 {
+      font-size: 1rem;
+    }
+
+    a {
+      padding: 0.3125rem 1rem;
+      font-size: 0.85rem;
+    }
+
+    p {
+      font-size: 0.75rem;
+    }
+  }
+}
+</style>
