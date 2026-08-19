fb88cf481ea96de3366358e0424b5db4db503057
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2026-08-18 20:12:26 -0500

    Start links constant

---
+++ src/constants/links.constant.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+/**
+ * Shared external links used across multiple pages/components
+ */
+
+export default {};
+
+export const Links = {
+  /** The City of Chicago's Energy Benchmarking Ordinance page */
+  ChicagoEnergyBenchmarkingOrdinance:
+    'https://www.chicago.gov/city/en/progs/env/building-energy-benchmarking' +
+    '---transparency.html',
+  /** The City of Chicago's 2022 Climate Action Plan page */
+  ChicagoClimateActionPlan:
+    'https://www.chicago.gov/city/en/sites/climate-action-plan/home.html',
+};
+</script>
